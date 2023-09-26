7b616c9ba639825bebc8f9dc09aa80459ea21db4
inovue <inouve3@gmail.com>
2023-09-26 15:39:48 +0900

    add: env site_url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3015',
+  siteUrl: 'https://'+ (process.env.NEXT_PUBLIC_SITE_URL || 'localhost:3000'),
   generateRobotsTxt: true, // (optional)
   // ...other options
   output: 'export',
