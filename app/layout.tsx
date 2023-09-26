70572028e7ffe65e959194eb4ca0fb389faca621
inovue <inouve3@gmail.com>
2023-09-26 14:43:52 +0900

    add: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Footer from './_components/Footer'
 import classNames from 'classnames'
 import dynamic from 'next/dynamic'
 import { metadata as meta } from './_config'
+import GoogleAnalytics from '@/app/_components/GoogleAnalytics'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +24,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="ja" className={classNames([yuji_syuku.variable])}>
       {process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID && <CrispChat />}
+      {process.env.NEXT_PUBLIC_GA_TRACKING_ID && <GoogleAnalytics />}
       <body className={classNames(/*[inter.className]*/)}>
         <Header className='sticky top-0' />
         <div className="h-full mb-auto pt-12">
