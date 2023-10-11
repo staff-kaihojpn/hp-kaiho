a04b564e59aacec5cc7010641d920fb4466f290b
inovue <inouve3@gmail.com>
2023-10-11 17:39:19 +0900

    dev commit

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,7 +1,6 @@
 import classNames from "classnames"
 import { IconType } from "react-icons"
 import Link from "next/link"
-import { config } from "../_config";
 
 type FloatingButtonProps = React.ComponentProps<typeof Link> & {
   icon: IconType;
@@ -12,19 +11,5 @@ export function FloatingButton({icon:Icon, className, ...props}: FloatingButtonP
     <Link {...props} aria-label="to line app" className={classNames("block rounded-full w-[54px] xs:w-[60px] aspect-square cursor-pointer shadow-md ", className)}>
       <Icon className="w-8 h-full m-auto"/>
     </Link>
-  )
-}
-
-export type ButtonProps = {
-  label?:string
-}
-export function LineButton({label="査定をはじめる"}:ButtonProps){
-  return (
-    <Link href={'/assessments/line'} className="block text-base py-2 px-4 bg-green-700 text-stone-50 rounded-full">{label}</Link>
-  )
-}
-export function ChatButton({label="チャットで相談"}:ButtonProps){
-  return (
-    <div onClick={config.action.openChat} className='text-base py-2 px-4 bg-stone-50 border-stone-300 border rounded-full cursor-pointer'>{label}</div>
   )
 }
